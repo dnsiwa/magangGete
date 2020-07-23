682372a1c42eb351d178905ec3698390a4a58350
dnsiwa <dionisiusiwa@gmail.com>
2020-07-23 19:41:58 +0700

    initial commit

---
+++ satu.js
@@ -0,0 +1,18 @@
+// console.log("ini contoh keluar text");
+// console.log(6*5);
+
+// function tambah(angkaPertama, angkaKedua){
+//     let hasil;
+//     hasil = angkaPertama + angkaKedua;
+
+//     return hasil;
+// }
+
+// console.log(hasil);
+// console.log(tambah(10, 20));
+
+// const satu = 1;
+// helloWorld = "Hello World"
+// // helloWorld = "ini Hello World Baru";
+// console.log(helloWorld);
+
